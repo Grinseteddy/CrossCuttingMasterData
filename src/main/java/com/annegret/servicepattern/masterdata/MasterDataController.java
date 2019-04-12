7e26dbaef82e4ba578d5e02754c0e6056252d9fe
Grinseteddy <annegret.kampe@arcor.de>
2019-04-12 17:12:29 +0200

    Master data with logging as json

--- src/main/java/com/annegret/servicepattern/masterdata/MasterDataController.java
+++ src/main/java/com/annegret/servicepattern/masterdata/MasterDataController.java
@@ -1,10 +1,14 @@
 package com.annegret.servicepattern.masterdata;
 
-import org.json.JSONException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 
 @RestController
@@ -14,12 +18,23 @@ public class MasterDataController {
 
     @GetMapping(value = "/Character/{index}")
     @ResponseBody
-    public MasterData masterDataAt(@PathVariable("index") int index) throws JSONException {
+    public MasterData masterDataAt(@PathVariable("index") String index) throws Exception {
 
-        MasterData answer=new MasterData(index);
+        try {
 
-        return answer;
+            MasterData answer = new MasterData(Integer.valueOf(index));
+            return answer;
 
+        } catch (Exception e) {
+            if (e.getClass() == ArrayIndexOutOfBoundsException.class) {
+                throw new ResponseStatusException
+                        (HttpStatus.NOT_FOUND, "Index must be between 0 and 26, index: " + String.valueOf(index));
+            }
+            else if(e.getClass() == NumberFormatException.class) {
+                throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, "Index must be an integer.");
+            }
+        }
+        return null;
     }
 }
 
